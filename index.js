88e70a335e40b014b6f45dd29feba0cf15859180
zelitosaide <zelitosaide83@gmail.com>
2022-11-11 12:28:55 +0200

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import { router as invoices } from "./bookkeeper/routes/invoices.js";
+// import { router as invoices } from "./bookkeeper/routes/invoices.js";
 
 const app = express();
 
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const CONNECTION_URL = process.env.CONNECTION_URL;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
 
 app.use("/invoices", invoices);
 
@@ -22,13 +22,17 @@ app.get("/", function (_, res) {
   res.send("--- Welcome to Bookkeeper API :) ---");
 });
 
-mongoose
-  .connect(CONNECTION_URL)
-  .then(function () {
-    app.listen(PORT, function () {
-      console.log(`Server running on port: ${PORT}`);
-    });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+app.listen(PORT, function () {
+  console.log(`Server running on port: ${PORT}`);
+});
+
+// mongoose
+//   .connect(CONNECTION_URL)
+//   .then(function () {
+//     app.listen(PORT, function () {
+//       console.log(`Server running on port: ${PORT}`);
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
