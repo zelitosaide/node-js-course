016a41388cfe04bfb46e555896661e6f469aedfb
zelitosaide <zelitosaide83@gmail.com>
2022-11-11 12:32:36 +0200

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-// import { router as invoices } from "./bookkeeper/routes/invoices.js";
+import { router as invoices } from "./bookkeeper/routes/invoices.js";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 // const CONNECTION_URL = process.env.CONNECTION_URL;
 
-// app.use("/invoices", invoices);
+app.use("/invoices", invoices);
 
 app.get("/", function (_, res) {
   res.send("--- Welcome to Bookkeeper API :) ---");
