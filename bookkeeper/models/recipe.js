e61257d6dc61749fc0c64887acb7202c053ee21c
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 18:45:25 +0200

    Recipes

---
+++ bookkeeper/models/recipe.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const recipeSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  imageUrl: String,
+  recipeItemsId: String,
+  steps: {
+    type: Array,
+    default: [],
+  },
+  time: Number,
+  authorId: String,
+  description: {
+    type: String,
+    default: "Some content...",
+  },
+  flag: Object,
+  createdAt: {
+    type: Date,
+    default: new Date().toISOString(),
+  },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export { Recipe };
