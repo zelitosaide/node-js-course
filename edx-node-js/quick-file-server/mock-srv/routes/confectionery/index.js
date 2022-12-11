ebe556b1261ac95b5532262e91cca5ac831327ed
zelitosaide <zelitosaide83@gmail.com>
2022-12-11 17:33:04 +0200

    edx nodejs course

--- edx-node-js/quick-file-server/mock-srv/routes/confectionery/index.js
+++ edx-node-js/quick-file-server/mock-srv/routes/confectionery/index.js
@@ -1,14 +1,35 @@
+// "use strict";
+
+// module.exports = async function (fastify, opts) {
+//   fastify.get("/", async function (request, reply) {
+//     return [
+//       {
+//         id: "B1",
+//         name: "Chocolate Bar",
+//         rrp: "22.40",
+//         info: "Delicious overpriced chocolate.",
+//       },
+//     ];
+//   });
+// };
+
 "use strict";
+
+const data = [
+  {
+    id: "B1",
+    name: "Chocolate Bar",
+    rrp: "22.40",
+    info: "Delicious overpriced chocolate.",
+  },
+];
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return [
-      {
-        id: "B1",
-        name: "Chocolate Bar",
-        rrp: "22.40",
-        info: "Delicious overpriced chocolate.",
-      },
-    ];
+    return data;
+  });
+  fastify.post("/", async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data);
+    return data;
   });
 };
--- edx-node-js/quick-file-server/mock-srv/routes/electronics/index.js
+++ edx-node-js/quick-file-server/mock-srv/routes/electronics/index.js
@@ -1,20 +1,47 @@
+// "use strict";
+
+// module.exports = async function (fastify, opts) {
+//   fastify.get("/", async function (request, reply) {
+//     return [
+//       {
+//         id: "A1",
+//         name: "Vacuum Cleaner",
+//         rrp: "99.99",
+//         info: "The suckiest vacuum in the world.",
+//       },
+//       {
+//         id: "A2",
+//         name: "Leaf Blower",
+//         rrp: "303.33",
+//         info: "This product will blow your socks off.",
+//       },
+//     ];
+//   });
+// };
+
 "use strict";
+
+const data = [
+  {
+    id: "A1",
+    name: "Vacuum Cleaner",
+    rrp: "99.99",
+    info: "The suckiest vacuum in the world.",
+  },
+  {
+    id: "A2",
+    name: "Leaf Blower",
+    rrp: "303.33",
+    info: "This product will blow your socks off.",
+  },
+];
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return [
-      {
-        id: "A1",
-        name: "Vacuum Cleaner",
-        rrp: "99.99",
-        info: "The suckiest vacuum in the world.",
-      },
-      {
-        id: "A2",
-        name: "Leaf Blower",
-        rrp: "303.33",
-        info: "This product will blow your socks off.",
-      },
-    ];
+    return data;
+  });
+  fastify.post("/", async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data);
+    return data;
   });
 };
