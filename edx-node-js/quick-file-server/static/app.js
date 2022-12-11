c805ce5455a5ea80f5dc4647ff1ee188df7841c4
zelitosaide <zelitosaide83@gmail.com>
2022-12-11 16:03:43 +0200

    edx nodejs course

--- edx-node-js/quick-file-server/static/app.js
+++ edx-node-js/quick-file-server/static/app.js
@@ -36,22 +36,39 @@
 
 const API = "http://localhost:3000";
 
-const populateProducts = async () => {
-  const products = document.querySelector("#products");
-  products.innerHTML = "";
-  const res = await fetch(API);
-  const data = await res.json();
+// const populateProducts = async () => {
+//   const products = document.querySelector("#products");
+//   products.innerHTML = "";
+//   const res = await fetch(API);
+//   const data = await res.json();
+//   for (const product of data) {
+//     const item = document.createElement("product-item");
+//     for (const key of ["name", "rrp", "info"]) {
+//       const span = document.createElement("span");
+//       span.slot = key;
+//       span.textContent = product[key];
+//       item.appendChild(span);
+//     }
+//     products.appendChild(item);
+//   }
+// };
+
+const populateProducts = async (category) => {
+  const products = document.querySelector('#products')
+  products.innerHTML = ''
+  const res = await fetch(${API}/${category})
+  const data = await res.json()
   for (const product of data) {
-    const item = document.createElement("product-item");
-    for (const key of ["name", "rrp", "info"]) {
-      const span = document.createElement("span");
-      span.slot = key;
-      span.textContent = product[key];
-      item.appendChild(span);
+    const item = document.createElement('product-item')
+    for (const key of ['name', 'rrp', 'info']) {
+      const span = document.createElement('span')
+      span.slot = key
+      span.textContent = product[key]
+      item.appendChild(span)
     }
-    products.appendChild(item);
+    products.appendChild(item)
   }
-};
+}
 
 // document.querySelector("#fetch").addEventListener("click", async () => {
 //   await populateProducts();
