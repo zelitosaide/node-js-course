8a2b3a0159973d2f952cf300cce950f1dc8d33cc
zelitosaide <zelitosaide83@gmail.com>
2022-12-11 13:59:59 +0200

    get started

--- node-tutorial/get-started/get-started.js
+++ node-tutorial/get-started/get-started.js
@@ -1,11 +1,11 @@
 import http from "http";
 
 const server = http.createServer(function (req, res) {
-  const host = req.headers.host;
-  const connection = req.headers.connection;
-  const cacheControl = req.headers["cache-control"];
-  const userAgent = req.headers["user-agent"];
-  const accept = req.headers.accept;
+  // const host = req.headers.host;
+  // const connection = req.headers.connection;
+  // const cacheControl = req.headers["cache-control"];
+  // const userAgent = req.headers["user-agent"];
+  // const accept = req.headers.accept;
 
   res.writeHead(200, {
     "Content-Type": "application/json",
@@ -13,11 +13,11 @@ const server = http.createServer(function (req, res) {
   res.end(
     JSON.stringify({
       method: req.method,
-      host,
-      connection,
-      cacheControl,
-      userAgent,
-      accept,
+      // host,
+      // connection,
+      // cacheControl,
+      // userAgent,
+      // accept,
       url: req.url,
       // req: req.headers,
       httpVersion: req.httpVersion,
