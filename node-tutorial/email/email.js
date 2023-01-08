90ba343763940a40ba6f4ce5c7ed3ecd5dbc10c2
zelitosaide <zelitosaide83@gmail.com>
2023-01-08 11:34:19 +0200

    Sending Email using Node.js

---
+++ node-tutorial/email/email.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+const mailOptions = {
+  from: process.env.EMAIL,
+  to: "zelito.saide@uem.mz",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
